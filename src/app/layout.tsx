9fd34eb5b634746ded33a8bf1d7c9322290fd7d8
parsTroy <troyparsons@hotmail.com>
2024-05-03 16:25:46 -0400

    Update layout for site header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "7Grim Studios.",
+  title: "7Grim | Home",
   description: "We make games.",
 };
 
