b602de9b8be8fa6b0b2f5e387347d85f4ebcfbd6
parsTroy <troyparsons@hotmail.com>
2024-05-22 12:38:57 -0400

    Base

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,5 @@
 const page = () => {
-  return <div>Projects GAME LINK Link to Games Pre For Games</div>;
+  return <div>Projects GAME LINK Link to Games Pre For Games Repeat Open</div>;
 };
 
 export default page;
