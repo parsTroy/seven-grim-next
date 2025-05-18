f0f43738d4504f294660591ad79a26f9b24a8cc3
parstroy <troyparsons@hotmail.com>
2025-05-18 11:10:31 -0400

    Fix syntax error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Game from "../app/components/Game.tsx";
+import Game from "../app/components/Game";
 
 export default function Home() {
   return (
